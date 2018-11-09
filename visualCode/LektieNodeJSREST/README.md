bb61fcc1a70aaba524174818b7dcefe372c5a660
Captain <mads130f@edu.easj.dk>
2018-11-09 09:37:25 +0100

    test mads

---
+++ visualCode/LektieNodeJSREST/README.md
@@ -0,0 +1,1 @@
+# NodeJSRESTAPIin10Min
---
+++ visualCode/socketIO2-billederAudio/pictureLoader.js
@@ -0,0 +1,12 @@
+
+
+function checkIfJPEG(message){
+    var count = message.search(".jpeg");
+   /*
+    if(count == -1){
+        return false;
+    }else{
+        return true;
+    }
+*/
+}
